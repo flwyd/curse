fa8ab3375b0167818d351f6267fc0fc97629490a
Trevor Stone <github@trevorstone.org>
2020-03-21 00:25:15 -0600

    Simplify type definition

--- curse.go
+++ curse.go
@@ -11,30 +11,28 @@ import (
 	"os"
 )
 
-type wordList struct {
-	words []string
-}
+type wordList []string
 
 // wordList's string representation is a random word from a list
-func (list *wordList) String() string {
-	return list.words[rand.Intn(len(list.words))]
+func (list wordList) String() string {
+	return list[rand.Intn(len(list))]
 }
 
 // lists of curse components
 var (
-	adjective1 = &wordList{[]string{"artless", "bawdy", "beslubbering",
-		"bootless", "brazen", "churlish", "cockered", "clouted", "craven",
-		"currish", "dankish", "dissembling", "distempered", "droning", "errant",
-		"fawning", "fitful", "fobbing", "froward", "frothy", "gleeking", "gnarling",
-		"goatish", "gorbellied", "greasy", "grizzled", "haughty", "hideous",
-		"impertinent", "infectious", "jaded", "jarring", "knavish", "lewd",
-		"loggerheaded", "lumpish", "mammering", "mangled", "mewling", "paunchy",
-		"peevish", "pernicious", "prating", "pribbling", "puking", "puny",
-		"purpled", "quailing", "queasy", "rank", "reeky", "roguish", "roynish",
-		"ruttish", "saucy", "sottish", "spleeny", "spongy", "surly", "tottering",
-		"unmuzzled", "vacant", "vain", "venomed", "villainous", "waggish", "wanton",
-		"warped", "wayward", "weedy", "wenching", "whoreson", "yeasty"}}
-	adjective2 = &wordList{[]string{"base-court", "bat-fowling", "beef-witted",
+	adjective1 = wordList([]string{"artless", "bawdy", "beslubbering", "bootless",
+		"brazen", "churlish", "cockered", "clouted", "craven", "currish", "dankish",
+		"dissembling", "distempered", "droning", "errant", "fawning", "fitful",
+		"fobbing", "froward", "frothy", "gleeking", "gnarling", "goatish",
+		"gorbellied", "greasy", "grizzled", "haughty", "hideous", "impertinent",
+		"infectious", "jaded", "jarring", "knavish", "lewd", "loggerheaded",
+		"lumpish", "mammering", "mangled", "mewling", "paunchy", "peevish",
+		"pernicious", "prating", "pribbling", "puking", "puny", "purpled",
+		"quailing", "queasy", "rank", "reeky", "roguish", "roynish", "ruttish",
+		"saucy", "sottish", "spleeny", "spongy", "surly", "tottering", "unmuzzled",
+		"vacant", "vain", "venomed", "villainous", "waggish", "wanton", "warped",
+		"wayward", "weedy", "wenching", "whoreson", "yeasty"})
+	adjective2 = wordList([]string{"base-court", "bat-fowling", "beef-witted",
 		"beetle-headed", "boil-brained", "bunched-backed", "clapper-clawed",
 		"clay-brained", "common-kissing", "crook-pated", "dismal-dreaming",
 		"dizzy-eyed", "dog-hearted", "dread-bolted", "earth-vexing", "elf-skinned",
@@ -50,21 +48,21 @@ var (
 		"shag-eared", "shard-borne", "sheep-biting", "shrill-gorged", "spur-galled",
 		"sour-faced", "swag-bellied", "tardy-gaited", "tickle-brained",
 		"toad-spotted", "unchin-snouted", "weak-hinged", "weather-bitten",
-		"white-livered"}}
-	noun = &wordList{[]string{"apple-john", "baggage", "barnacle", "bladder",
+		"white-livered"})
+	noun = wordList([]string{"apple-john", "baggage", "barnacle", "bladder",
 		"boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish",
 		"clotpole", "coxcomb", "codpiece", "crutch", "cutpurse", "death-token",
 		"dewberry", "dogfish", "egg-shell", "flap-dragon", "flax-wench",
 		"flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon",
-		"gull-catcher", "haggard", "harpy", "hedge-pig", "hempseed",
-		"horn-beast", "hugger-mugger", "jack-a-nape", "jolthead", "lewdster",
-		"lout", "maggot-pie", "malignancy", "malkin", "malt-worm", "mammet",
-		"manikin", "measle", "minimus", "minnow", "miscreant", "moldwarp",
-		"mumble-news", "nut-hook", "pantaloon", "pigeon-egg", "pignut", "puttock",
-		"pumpion", "rabbit-sucker", "rampallion", "ratsbane", "remnant", "rudesby",
-		"ruffian", "scantling", "scullion", "scut", "skainsmate", "snipe",
-		"strumpet", "varlot", "vassal", "waterfly", "whey-face", "whipster",
-		"wagtail", "younker"}}
+		"gull-catcher", "haggard", "harpy", "hedge-pig", "hempseed", "horn-beast",
+		"hugger-mugger", "jack-a-nape", "jolthead", "lewdster", "lout",
+		"maggot-pie", "malignancy", "malkin", "malt-worm", "mammet", "manikin",
+		"measle", "minimus", "minnow", "miscreant", "moldwarp", "mumble-news",
+		"nut-hook", "pantaloon", "pigeon-egg", "pignut", "puttock", "pumpion",
+		"rabbit-sucker", "rampallion", "ratsbane", "remnant", "rudesby", "ruffian",
+		"scantling", "scullion", "scut", "skainsmate", "snipe", "strumpet",
+		"varlot", "vassal", "waterfly", "whey-face", "whipster", "wagtail",
+		"younker"})
 )
 
 // function to generate one curse
